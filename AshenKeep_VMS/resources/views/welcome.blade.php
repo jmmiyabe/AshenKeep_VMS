ac1451be107339c35ce63712eaae740679f38a6b
John Keisuke Miyabe <120996624+jmmiyabe@users.noreply.github.com>
2024-12-25 13:55:47 +0000

    removed the footer

--- AshenKeep_VMS/resources/views/welcome.blade.php
+++ AshenKeep_VMS/resources/views/welcome.blade.php
@@ -93,10 +93,6 @@
                                     />
                         </div>
                     </main>
-
-                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </footer>
                 </div>
             </div>
         </div>
