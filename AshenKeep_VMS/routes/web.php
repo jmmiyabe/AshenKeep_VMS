c50373ea555b70f912187fcf86f1c663cab80458
hsusana <152581281+hsusana20@users.noreply.github.com>
2024-12-25 12:32:30 +0000

    Laravel setup and update bg

---
+++ AshenKeep_VMS/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+$url = config('app.url');
+URL::forceRootUrl($url);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('applicant_requirements');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
