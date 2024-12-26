10c9168ae19cf19c7a66dc591b1acc460f73ee6c
John Keisuke Miyabe <120996624+jmmiyabe@users.noreply.github.com>
2024-12-26 12:11:27 +0000

    updated the login and register screen css

--- AshenKeep_VMS/resources/views/components/authentication-card.blade.php
+++ AshenKeep_VMS/resources/views/components/authentication-card.blade.php
@@ -1,4 +1,5 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#F6F2E9] dark:bg-gray-900"
+     style="background-image: url('./img/login_bg.svg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
     <div>
         {{ $logo }}
     </div>
