b084e0f73bc7e34d219a0aa804d89aa421a248c2
jmmiyabe <jmmiyabe@student.apc.edu.ph>
2024-12-28 19:08:35 +0800

    Reverted the welcome page

--- AshenKeep_VMS/resources/views/welcome.blade.php
+++ AshenKeep_VMS/resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-[#102A45] ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Sign Up
+                                            Register
                                         </a>
                                     @endif
                                 @endauth
